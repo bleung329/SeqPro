80a4cfd5c41adf4d888e20a535bc4c6d69a5dced
bleung329 <bleung@stuy.edu>
2017-12-08 14:49:17 -0500

    Some more stuff happened.

--- list.js
+++ list.js
@@ -1,10 +1,25 @@
 var additem = function(){
-	console.log("its firing");
-	var thelist = document.getElementById("thelist");
+	var list = document.getElementById("thelist").getElementsByTagName("li");
+	var newNum = list.length;
 	var newElem = document.createElement("li");
-	newElem.innerHTML = "some stuff";
+	newElem.innerHTML = "item "+newNum;
+	newElem.addEventListener("mouseover",modHeader);
+	var thelist = document.getElementById("thelist");
 	thelist.appendChild(newElem);
 };
+
+var modHeader = function(header)
+{
+	console.log(this.innerHTML);
+	console.log(header.innerHTML);
+	header.innerHTML = this.innerHTML;
+	/*
+	console.log("Does work?");
+	var replace = document.createElement('h1');
+	replace.innerHTML = this.innerHTML;
+	header.replaceChild(replace,header);
+	*/
+}
 
 var changeheader = function(blah)
 {
@@ -14,8 +29,17 @@ var changeheader = function(blah)
 	header.replaceChild(wow,header);
 }
 
+var list = document.getElementById("thelist").getElementsByTagName("li");
+for (var entry of list.entries())
+{
+	entry.addEventListener("mouseover",modHeader);
+	//entry.addEventListener("",modNormal);
+}
+
 var wow = document.getElementById("b");
 wow.addEventListener('click', additem);
+var header = document.getElementById("h");
+
 
 //var wow2 = document.getElementById("h");
-document.getElementById("thelist").onmouseover = function() {changeheader("testing")};
+//document.getElementById("thelist").onmouseover = function() {changeheader("testing")};
